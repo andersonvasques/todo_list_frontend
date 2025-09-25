bd72ec3bc561e55971da966187dc4eaa1b3509c9
Anderson Vasques <anderson.limav9@gmail.com>
2025-09-25 15:23:41 -0300

    feat: created input component to the auth pages

---
+++ src/components/Inputs/InputComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-input
+    class="col-12 q-ma-md"
+    :type="type"
+    :label="label"
+    color="white"
+    label-color="white"
+    :rules="rules"
+    dense
+    :model-value="modelValue"
+    @update:model-value="updateValue"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{
+  modelValue: string;
+  type?: 'text' | 'password' | 'email';
+  label?: string;
+  color?: string;
+  labelColor?: string;
+  rules?: Array<(val: string) => true | string>;
+  dense?: boolean;
+}>();
+
+const emits = defineEmits(['update:modelValue']);
+
+function updateValue(val: string | number | null) {
+  emits('update:modelValue', val);
+}
+</script>
+
+<script lang="ts">
+export default {
+  name: 'InputNameComponent',
+};
+</script>
